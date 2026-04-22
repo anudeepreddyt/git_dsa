f5ca3d1c10950a55dd1a8a355c3f9bf17b0fb8a2
anudeepreddyt <anudeepreddy4637@gmail.com>
2026-04-22 13:14:11 +0530

    Updated

---
+++ kadanes.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class kadanes {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+
+        int maxSum=Integer.MIN_VALUE;
+        int asStart=-1;
+        int asEnd=-1;
+        int start=0;
+        int maxLen=0;
+        int sum=0;
+
+
+        for(int i=0;i<n;i++)
+        {
+            if(sum==0)
+                start=i;
+
+            sum=sum+arr[i];
+
+            if(sum>maxSum)
+            {
+                asStart=start;
+                asEnd=i;
+                maxSum=sum;
+            }
+
+            if(sum<0)
+                sum=0;
+        }
+
+        maxLen=asEnd-asStart+1;
+
+        System.out.println("No.of elements in SubArray : "+maxLen);
+        System.out.println("Max sum of the SubArray : "+ maxSum);
+
+        for(int i=asStart;i<=asEnd;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
