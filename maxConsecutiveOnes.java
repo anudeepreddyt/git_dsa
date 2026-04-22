265bbf0289fd125a925cba4257eba49f969cf4e0
anudeepreddyt <anudeepreddy4637@gmail.com>
2026-04-22 14:02:38 +0530

    Updated

--- maxConsecutiveOnes.java
+++ maxConsecutiveOnes.java
@@ -1,3 +1,23 @@
+/*
+Description:
+Finds the maximum number of consecutive 1s present in the array and prints the longest streak.
+
+Approach:
+
+Take array size and elements as input.
+Traverse the array element by element.
+If the current element is 1, increase the count.
+Update the maximum count whenever a longer streak is found.
+If the current element is not 1, reset the count to zero.
+Print the maximum consecutive count.
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+*/
+
+
 import java.util.*;
 
 public class maxConsecutiveOnes{
